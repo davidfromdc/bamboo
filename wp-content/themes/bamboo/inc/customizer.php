f04bf964da981b66948b9dab159954470767aeb6
davidfromdc <davidlewis@Davids-MacBook-Pro-2.local>
2016-02-22 18:17:48 -0500

    Added Bamboo Theme to Wordpress

---
+++ wp-content/themes/bamboo/inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Bamboo Theme Customizer.
+ *
+ * @package Bamboo
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function bamboo_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'bamboo_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function bamboo_customize_preview_js() {
+	wp_enqueue_script( 'bamboo_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'bamboo_customize_preview_js' );
